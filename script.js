62cc30d0bf8a0c731ff1c0e915613ae14cd12e60
Rohit Prajapati <65508466+rohu0510@users.noreply.github.com>
2023-01-13 21:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	const mapping = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
+    let result = [''];
+    for (let digit of input_digit) {
+        let temp = [];
+        for (let letter of mapping[digit]) {
+            for (let combination of result) {
+                temp.push(combination + letter);
+            }
+        }
+        result = temp;
+    }
+    return result.sort();
 }
 
 module.exports = letterCombinations;
